4b989a6728f0cff82bf98552734adc0373719748
fazlulkarimhridoy <fkhridoy4321@gmail.com>
2024-03-11 22:42:32 +0600

    updated cors

--- index.js
+++ index.js
@@ -10,7 +10,10 @@ const port = process.env.PORT || 5000;
 // middlewares
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: [
+      "http://localhost:5173",
+      "https://local-tour-client.vercel.app"
+    ],
     credentials: true,
   })
 );
